2fb488788347dd54fd207b7ba27b3d12be7ceed2
Nikita Vasilev <nv3212@gmail.com>
2024-01-25 19:47:41 +0100

    Add support for Swift 5.7 and Swift 5.8

---
+++ Package@swift-5.7.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Blade",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v11),
+        .tvOS(.v13),
+        .watchOS(.v7),
+    ],
+    products: [
+        .library(name: "Blade", targets: ["Blade"]),
+        .library(name: "BladeTCA", targets: ["BladeTCA"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", .upToNextMajor(from: "1.5.5")),
+    ],
+    targets: [
+        .target(name: "Blade"),
+        .target(
+            name: "BladeTCA",
+            dependencies: [
+                "Blade",
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+            ]
+        ),
+        .testTarget(name: "BladeTests", dependencies: ["Blade"]),
+    ]
+)
