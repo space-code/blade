680de16d7644c10af63fcb4b704817e2bd4882b2
Nikita Vasilev <nv3212@gmail.com>
2024-08-26 09:45:08 +0400

    Integrate header and footer views into the list view

--- Sources/BladeTCA/Classes/Presentation/ViewModifiers/LoadingViewModifier.swift
+++ Sources/BladeTCA/Classes/Presentation/ViewModifiers/LoadingViewModifier.swift
@@ -13,7 +13,7 @@ struct LoadingViewModifier: ViewModifier {
     // MARK: ViewModifier
 
     func body(content: Content) -> some View {
-        VStack {
+        Group {
             content
 
             if isLoading {
